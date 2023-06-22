ebd1d53723d9cc316c93c1966d5c46d0071583f1
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:01:48 +0200

    feat: create ProductTableBodyRow component

---
+++ components/ProductTableBodyRow.vue
@@ -0,0 +1,26 @@
+<template>
+    <TableBodyRow>
+        <TableCell>{{ id }}</TableCell>
+        <TableCell>{{ name }}</TableCell>
+        <TableCell>{{ category }}</TableCell>
+        <TableCell>{{ fitterPlace }}</TableCell>
+        <TableCell>{{ price }} PLN</TableCell>
+        <TableCell>{{ quantity }} szt.</TableCell>
+        <TableCell>
+            <TableBodyRowActions />
+        </TableCell>
+    </TableBodyRow>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+    id: number,
+    name: string,
+    category: string,
+    fitterPlace: string,
+    price: number,
+    quantity: number
+}
+
+defineProps<Props>()
+</script>
