1ee3d82f075949b365c3e8636b7d11500919b64a
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:01:16 +0200

    feat: create SearchResults component

---
+++ components/SearchResults.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="m-8 flex flex-row gap-x-8">
+        <Box class="w-full">
+            <BoxTitle>produkty</BoxTitle>
+            <div class="mt-8 flex flex-col gap-y-4">
+                <SearchResultItem :id="123" name="Fajny domek" />
+                <SearchResultItem :id="321" name="Fajny domek" />
+                <SearchResultItem :id="432" name="Fajny domek" />
+            </div>
+        </Box>
+        <Box class="w-full">
+            <BoxTitle>produkty</BoxTitle>
+            <p class="text-l mt-8 normalcase text-gray-400">Brak wyników wyszukiwania</p>
+        </Box>
+    </div>
+</template>
