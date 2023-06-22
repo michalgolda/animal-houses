f87c55ab564e4ec41f5056dda152cae7b7af6cef
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:57:22 +0200

    feat: create SearchResultItem component

---
+++ components/SearchResultItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-row items-center hover:bg-gray-100 cursor-pointer bg-gray-50 gap-x-4 w-full p-4">
+        <span class="text-base text-slate-700">{{ id }}</span>
+        <span class="text-base text-slate-700 grow">{{ name }}</span>
+        <img src="arrow-box.svg" width="32" height="32" />
+    </div>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+    id: number,
+    name: string
+}
+
+defineProps<Props>()
+</script>
