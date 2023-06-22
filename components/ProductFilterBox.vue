5383e16e75ed6574ba29ff1623a60d8dfef54b11
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:01:30 +0200

    feat: create ProductFilterBox component

---
+++ components/ProductFilterBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <Box class="w-96" v-bind="$attrs">
+        <BoxTitle>filtry</BoxTitle>
+        <div class="mt-4 flex flex-col gap-y-4">
+            <Field is="select" name="category" label="Kategoria">
+                <option>Psy</option>
+                <option>Koty</option>
+            </Field>
+            <Field is="select" name="fitter-place" label="Miejsce montażu">
+                <option>Drzewo</option>
+                <option>Ziemia</option>
+            </Field>
+            <Field is="select" name="isolation" label="Izolacja">
+                <option>Wewnętrzna</option>
+                <option>Zewnętrzna</option>
+            </Field>
+            <PrimaryButton>Zastouj</PrimaryButton>
+            <SecondaryButton>Zresetuj</SecondaryButton>
+        </div>
+    </Box>
+</template>
