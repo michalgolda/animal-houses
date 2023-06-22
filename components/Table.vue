6a37676fde962bd7baa5d014b46e52899b9a864b
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:53:06 +0200

    feat: create Table component

---
+++ components/Table.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="table w-full normal-case border-separate border-spacing-y-4" v-bind="$attrs">
+        <div class="table-header-group">
+            <slot name="header"></slot>
+        </div>
+        <div class="table-row-group">
+            <slot name="body"></slot>
+        </div>
+    </div>
+</template>
