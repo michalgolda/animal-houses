72a8711d90cb350b86af49e2974359054c7b37f5
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:54:10 +0200

    feat: create TableHeadCell component

---
+++ components/TableHeadCell.vue
@@ -0,0 +1,20 @@
+<template>
+    <TableCell class="!pt-0 !pb-0 text-left font-semibold text-sm text-tertiary" v-bind="$attrs">
+        <div class="flex flex-row items-center">
+            <span>
+                <slot></slot>
+            </span>
+            <IconButton v-if="showSortButton" iconSrc="sort-column.svg" />
+        </div>
+    </TableCell>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+    showSortButton?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+    showSortButton: true
+})
+</script>
