c912ae48e218ed62cda1ace43edf75023e8c1729
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:03:40 +0200

    feat: create CategoryBox component

---
+++ components/CategoryBox.vue
@@ -0,0 +1,9 @@
+<template>
+    <Box>
+        <div class="flex flex-row align-start justify-between">
+            <BoxTitle>kategorie</BoxTitle>
+            <PrimaryButton>Dodaj kategorię</PrimaryButton>
+        </div>
+        <CategoryTable class="mt-2" />
+    </Box>
+</template>
