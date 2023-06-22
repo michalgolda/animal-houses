f338381e88bc2cdf33ed2c419de023234003c798
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:02:03 +0200

    feat: create ProductTableHeaderRow component

---
+++ components/ProductTableHeaderRow.vue
@@ -0,0 +1,11 @@
+<template>
+    <TableRow>
+        <TableHeadCell class="!py-2">ID</TableHeadCell>
+        <TableHeadCell class="!py-2">Nazwa produktu</TableHeadCell>
+        <TableHeadCell class="!py-2">Kategoria</TableHeadCell>
+        <TableHeadCell class="!py-2">Miejsce montażu</TableHeadCell>
+        <TableHeadCell class="!py-2">Cena</TableHeadCell>
+        <TableHeadCell class="!py-2">Ilość</TableHeadCell>
+        <TableHeadCell class="!py-2" :show-sort-button="false"></TableHeadCell>
+    </TableRow>
+</template>
