3076ffb9de9feebb1fe266e7fe3248331a42d540
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:49:21 +0200

    feat: create BaseButton component

---
+++ components/BaseButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button v-bind="$attrs"
+        class="font-semibold inline-flex items-center gap-x-2 leading-tight justify-center rounded px-8 py-2 text-sm">
+        <slot></slot>
+    </button>
+</template>
