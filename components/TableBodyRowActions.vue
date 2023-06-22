49ced8a5e735e0520abc71fe680da3dacc5797b9
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:54:29 +0200

    feat: create TableBodyRowActions component

---
+++ components/TableBodyRowActions.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-row gap-x-4 justify-end items-center h-full" v-bind="$attrs">
+        <IconButton iconSrc="brush.svg" />
+        <IconButton iconSrc="trash.svg" />
+    </div>
+</template>
