1e15ac4472d00cc8234e87d677cc2e240f4c4c95
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:03:29 +0200

    feat: create CategoryTable component

---
+++ components/CategoryTable.vue
@@ -0,0 +1,22 @@
+<template>
+    <Table>
+        <template v-slot:header>
+            <CategoryTableHeaderRow />
+        </template>
+        <template v-slot:body>
+            <CategoryTableBodyRow v-for="category in categories" :key="category.id" :id="category.id" :name="category.name"
+                :productCount="category.productCount" :createdAt="category.createdAt" />
+        </template>
+    </Table>
+</template>
+
+<script setup lang="ts">
+const categories = ref([
+    {
+        id: 123,
+        name: 'Psy',
+        productCount: 16,
+        createdAt: '18.02.2024',
+    },
+])
+</script>
