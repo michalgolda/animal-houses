d06f3404bd04bb90131f2a14eef29925b8734be8
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:02:57 +0200

    feat: create CategoryTableHeaderRow component

---
+++ components/CategoryTableHeaderRow.vue
@@ -0,0 +1,9 @@
+<template>
+    <TableRow>
+        <TableHeadCell class="!py-2">ID</TableHeadCell>
+        <TableHeadCell class="!py-2">Nazwa kategorii</TableHeadCell>
+        <TableHeadCell class="!py-2">Ilość produktów</TableHeadCell>
+        <TableHeadCell class="!py-2">Data utworzenia</TableHeadCell>
+        <TableHeadCell class="!py-2" :show-sort-button="false"></TableHeadCell>
+    </TableRow>
+</template>
