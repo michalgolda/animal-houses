7b2d19e6fbd07d8569f99a37ce6250c7a088e433
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:50:44 +0200

    feat: create PrimaryOutlinedButton component

---
+++ components/PrimaryOutlinedButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <BaseButton v-bind="$attrs"
+        class="text-orange-500 border border-solid border-orange-500 hover:bg-orange-500 hover:text-white">
+        <slot></slot>
+    </BaseButton>
+</template>
