5a7f78adcfa2e5ce36d653725525da1f2c978340
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:03:15 +0200

    feat: create CategoryTableBodyRow component

---
+++ components/CategoryTableBodyRow.vue
@@ -0,0 +1,25 @@
+<template>
+    <TableBodyRow>
+        <TableCell>{{ id }}</TableCell>
+        <TableCell>{{ name }}</TableCell>
+        <TableCell>{{ productCount }}</TableCell>
+        <TableCell>{{ createdAt }}</TableCell>
+        <TableCell>
+            <div class="flex flex-row gap-x-4 justify-end items-center h-full">
+                <IconButton iconSrc="brush.svg" />
+                <IconButton iconSrc="trash.svg" />
+            </div>
+        </TableCell>
+    </TableBodyRow>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+    id: number,
+    name: string,
+    productCount: number,
+    createdAt: string,
+}
+
+defineProps<Props>()
+</script>
