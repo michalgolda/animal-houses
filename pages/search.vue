385b0fd129081656bbbc177b9262572a437530fb
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:04:48 +0200

    feat: create search page

---
+++ pages/search.vue
@@ -0,0 +1,18 @@
+<template>
+    <NuxtLayout name="search">
+        <Box class="w-full">
+            <BoxTitle>produkty</BoxTitle>
+            <p class="normal-case mt-4">Brak wyników wyszukiwania</p>
+        </Box>
+        <Box class="w-full">
+            <BoxTitle>kategorie</BoxTitle>
+            <p class="normal-case mt-4">Brak wyników wyszukiwania</p>
+        </Box>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: 'AnimalHouses - Search'
+})
+</script>
