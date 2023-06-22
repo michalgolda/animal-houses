4c522d989dfc8cfc7e5a276d25711de5352cf812
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:02:16 +0200

    feat: create ProductTable component

---
+++ components/ProductTable.vue
@@ -0,0 +1,25 @@
+<template>
+    <Table>
+        <template v-slot:header>
+            <ProductTableHeaderRow />
+        </template>
+        <template v-slot:body>
+            <ProductTableBodyRow v-for="product in products" :key="product.id" :id="product.id" :name="product.name"
+                :category="product.category" :fitterPlace="product.fitterPlace" :price="product.price"
+                :quantity="product.quantity" />
+        </template>
+    </Table>
+</template>
+
+<script setup lang="ts">
+const products = ref([
+    {
+        id: 123,
+        name: 'Fajny domek',
+        category: 'Psy',
+        fitterPlace: 'Ziemia',
+        price: 120,
+        quantity: 1200
+    }
+])
+</script>
