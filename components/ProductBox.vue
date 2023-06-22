25d3e6490ea5ab90ff66c464e8acad4788d2797e
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 22:02:27 +0200

    feat: create ProductBox component

---
+++ components/ProductBox.vue
@@ -0,0 +1,9 @@
+<template>
+    <Box class="w-full">
+        <div class="flex flex-row justify-between items-start">
+            <BoxTitle>produkty</BoxTitle>
+            <PrimaryButton iconSrc="plus-box.svg">Dodaj produkt</PrimaryButton>
+        </div>
+        <ProductTable class="mt-2" />
+    </Box>
+</template>
