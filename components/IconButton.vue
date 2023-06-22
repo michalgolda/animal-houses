336161b5443484652b62daf3015cea1f6344fb38
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:49:49 +0200

    feat: create IconButton component

---
+++ components/IconButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <BaseButton class="!py-1 !px-1" v-bind="$attrs">
+        <img class="w-6 h-6" style="min-width: 16px;" :src="iconSrc" width="24" height="24" />
+    </BaseButton>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+    iconSrc: string
+}
+
+defineProps<Props>()
+</script>
