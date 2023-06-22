6fa7bfdf90a3182b53bd92e845522e88bf5d7e7d
Michał Gołda <michal.golda@hotmail.com>
2023-06-22 21:52:53 +0200

    feat: create Menu component

---
+++ components/Menu.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="columns-2 gap-0">
+        <MenuItem to="/products" name="Produkty" iconSrc="products.svg" />
+        <MenuItem to="/categories" name="Kategorie" iconSrc="categories.svg" />
+    </div>
+</template>
