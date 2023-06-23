5fa71e51c9fa87d37235cd6499aa84f58e58ea52
Michał Gołda <michal.golda@hotmail.com>
2023-06-23 20:08:04 +0200

    refactor: create TableHeadRow component

---
+++ components/TableHeadRow.vue
@@ -0,0 +1,8 @@
+<template>
+    <TableRow>
+        <slot></slot>
+        <TableHeadCell class="!py-2" :show-sort-button="false" justify-right>
+            <IconButton icon-src="settings.svg" width="24" height="24" />
+        </TableHeadCell>
+    </TableRow>
+</template>
