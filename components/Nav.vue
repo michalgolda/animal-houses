4dfbfad10f9ab532a055e63625aa9896db49a17b
Michał Gołda <michal.golda@hotmail.com>
2023-06-29 02:34:01 +0200

    fix: img src path have to slash as  prefix

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="flex px-8 py-4 bg-white items-center w-full">
-        <img src="logo.svg" alt="logo" />
+        <img src="/logo.svg" alt="logo" />
         <SearchForm />
         <CurrencySelect class="ml-4" />
     </nav>
