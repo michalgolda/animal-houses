5a027129c6d0461062b7330ee2f1b9ffb369a37e
Michał Gołda <michal.golda@hotmail.com>
2023-06-30 22:23:33 +0200

    feat: load default attributes and products from json files

--- composables/attribute/useAttributeStorage.ts
+++ composables/attribute/useAttributeStorage.ts
@@ -1,27 +1,4 @@
+import defaultAttributes from "~/assets/defaultAttributes.json";
+
 export const useAttributeStorage = () =>
-  useStorage("productAttributes", [
-    {
-      id: getShortId(),
-      name: "Kategoria",
-      type: "select",
-      valueIndex: null,
-      values: ["Psy", "Koty", "Sarny", "Szczury"],
-      createdAt: "2023-05-19",
-    },
-    {
-      id: getShortId(),
-      name: "Izolacja",
-      type: "select",
-      valueIndex: null,
-      values: ["Wewnętrzna", "Zewnętrzna"],
-      createdAt: getCurrentDateString(),
-    },
-    {
-      id: getShortId(),
-      name: "Miejsce montażu",
-      type: "select",
-      valueIndex: null,
-      values: ["Ziemia", "Drzewo"],
-      createdAt: getCurrentDateString(),
-    },
-  ]);
+  useStorage("attribute", [...defaultAttributes]);
--- composables/product/useProductStorage.ts
+++ composables/product/useProductStorage.ts
@@ -1,33 +1,4 @@
-import shortid from "shortid";
+import defaultProducts from "~/assets/defaultProducts.json";
 
-export const useProductStorage = () => {
-  const attributeStorage = useAttributeStorage();
-
-  const defaultAttributes = {};
-  for (const attribute of attributeStorage.entities.value) {
-    defaultAttributes[attribute.id] = attribute.values[0];
-  }
-
-  const productStorage = useStorage<Product>("products", [
-    {
-      id: shortid(),
-      name: "Domek dwuczęściowy kolorowy",
-      description: "Fajny domek",
-      price: 120,
-      quantity: 90,
-      attributes: defaultAttributes,
-      createdAt: getCurrentDateString(),
-    },
-    {
-      id: shortid(),
-      name: "Domek dwuczęściowy kolorowy",
-      description: "Fajny domek",
-      price: 1111,
-      quantity: 900,
-      attributes: defaultAttributes,
-      createdAt: getCurrentDateString(),
-    },
-  ]);
-
-  return productStorage;
-};
+export const useProductStorage = () =>
+  useStorage("product", [...defaultProducts]);
